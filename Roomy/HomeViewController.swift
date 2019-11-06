15683f293892179e27e5cc9b9354ce9948fd6199
ahmedbahgaten <ahmedbahgatnabih@gmail.com>
2019-11-06 06:17:54 -0800

    last

--- Roomy/HomeViewController.swift
+++ Roomy/HomeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class HomeVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    let street = ["35 Pierrepont St #C7","86 Gerrymain Rd #F3","22 Yonker St #B4" , "90 Simmons Rd #M3"]
+    let street = ["34 Pierrepont St #C7","86 Gerrymain Rd #F3","22 Yonker St #B4" , "90 Simmons Rd #M3"]
     let price = ["2,500$","1,250$","4,200$","2,700$"]
     let address = ["Brooklyn, NY 11236","NewYork NY 11253","Queens,NY 11539","Brooklyn,NY11236"]
     let FlatImages :[UIImage] = [#imageLiteral(resourceName: "a63ccac00c7d621857898eb04330f303a55cea72"),#imageLiteral(resourceName: "ba247016de035e2c7b6312457302117c316298d9"),#imageLiteral(resourceName: "5f9d2a8019b7de7e771dbe18f761af282acabb19"),#imageLiteral(resourceName: "a2c73f37021cdb119855041400d89162a5c07765"),#imageLiteral(resourceName: "92103a27b2f3f0f0f0f49f940861d74824895162"),#imageLiteral(resourceName: "92103a27b2f3f0f0f0f49f940861d74824895162"),#imageLiteral(resourceName: "24d1ad730cd767672565497842121bb04a02f7d9"),#imageLiteral(resourceName: "a63ccac00c7d621857898eb04330f303a55cea72"),#imageLiteral(resourceName: "5f9d2a8019b7de7e771dbe18f761af282acabb19"),#imageLiteral(resourceName: "a2c73f37021cdb119855041400d89162a5c07765"),#imageLiteral(resourceName: "a63ccac00c7d621857898eb04330f303a55cea72")]
